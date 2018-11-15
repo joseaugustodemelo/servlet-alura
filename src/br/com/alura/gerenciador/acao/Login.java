1dfd8b223bc0722d0ead14400121feeb8b894679
José Augusto de Melo <joseaugustodemelo@gmail.com>
2018-11-15 19:03:25 -0200

    Initial commit
    Changes to be committed:
    	new file:   .classpath
    	new file:   .project
    	new file:   .settings/.jsdtscope
    	new file:   .settings/org.eclipse.jdt.core.prefs
    	new file:   .settings/org.eclipse.wst.common.component
    	new file:   .settings/org.eclipse.wst.common.project.facet.core.xml
    	new file:   .settings/org.eclipse.wst.jsdt.ui.superType.container
    	new file:   .settings/org.eclipse.wst.jsdt.ui.superType.name
    	new file:   WebContent/META-INF/MANIFEST.MF
    	new file:   WebContent/WEB-INF/lib/jstl-1.2.jar
    	new file:   WebContent/WEB-INF/view/formAlteraEmpresa.jsp
    	new file:   WebContent/WEB-INF/view/formLogin.jsp
    	new file:   WebContent/WEB-INF/view/formNovaEmpresa.jsp
    	new file:   WebContent/WEB-INF/view/listaEmpresas.jsp
    	new file:   WebContent/WEB-INF/view/novaEmpresaCriada.jsp
    	new file:   WebContent/WEB-INF/web.xml
    	new file:   WebContent/bem-vindo.html
    	new file:   build/classes/br/com/alura/gerenciador/acao/Acao.class
    	new file:   build/classes/br/com/alura/gerenciador/acao/AlteraEmpresa.class
    	new file:   build/classes/br/com/alura/gerenciador/acao/ListaEmpresas.class
    	new file:   build/classes/br/com/alura/gerenciador/acao/Login.class
    	new file:   build/classes/br/com/alura/gerenciador/acao/LoginForm.class
    	new file:   build/classes/br/com/alura/gerenciador/acao/MostraEmpresa.class
    	new file:   build/classes/br/com/alura/gerenciador/acao/NovaEmpresa.class
    	new file:   build/classes/br/com/alura/gerenciador/acao/NovaEmpresaForm.class
    	new file:   build/classes/br/com/alura/gerenciador/acao/RemoveEmpresa.class
    	new file:   build/classes/br/com/alura/gerenciador/modelo/Banco.class
    	new file:   build/classes/br/com/alura/gerenciador/modelo/Empresa.class
    	new file:   build/classes/br/com/alura/gerenciador/modelo/Usuario.class
    	new file:   build/classes/br/com/alura/gerenciador/servlet/OiMundoServlet.class
    	new file:   build/classes/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.class
    	new file:   src/br/com/alura/gerenciador/acao/Acao.java
    	new file:   src/br/com/alura/gerenciador/acao/AlteraEmpresa.java
    	new file:   src/br/com/alura/gerenciador/acao/ListaEmpresas.java
    	new file:   src/br/com/alura/gerenciador/acao/Login.java
    	new file:   src/br/com/alura/gerenciador/acao/LoginForm.java
    	new file:   src/br/com/alura/gerenciador/acao/MostraEmpresa.java
    	new file:   src/br/com/alura/gerenciador/acao/NovaEmpresa.java
    	new file:   src/br/com/alura/gerenciador/acao/NovaEmpresaForm.java
    	new file:   src/br/com/alura/gerenciador/acao/RemoveEmpresa.java
    	new file:   src/br/com/alura/gerenciador/modelo/Banco.java
    	new file:   src/br/com/alura/gerenciador/modelo/Empresa.java
    	new file:   src/br/com/alura/gerenciador/modelo/Usuario.java
    	new file:   src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
    	new file:   src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java

---
+++ src/br/com/alura/gerenciador/acao/Login.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+public class Login implements Acao{
+	
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+					
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		System.out.println("Logando " + login);
+		
+		return "redirect:entrada?acao=ListaEmpresas";
+	}
+
+}
